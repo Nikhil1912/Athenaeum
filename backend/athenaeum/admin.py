d5e6876d7e90c930fcc66c1c7de8ade86b545637
insamuel <i_samuelsson@yahoo.com>
2022-09-23 20:55:01 -0400

    GIT-31 added user login model

--- backend/athenaeum/admin.py
+++ backend/athenaeum/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Book
+from .models import Login
+
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'authors', 'description', 'isInStock')
 
+
 # Register your models here.
 admin.site.register(Book, BookAdmin)
+admin.site.register(Login)
