b91793aed3f768c3f222d59d0ec202137774c6f4
M M Abid Naziri <abidnazirisami@gmail.com>
2022-10-03 15:00:04 -0400

    GIT-57 Fixed the bug for optional fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -61,7 +61,7 @@ class App extends Component {
   };
 
   createItem = () => {
-    const item = { title: "", authors: "", description: "", isInStock: false };
+    const item = { title: "", authors: "", ispn: "", description: "", condition: "", price: "", linkToBuy: "", isInStock: false };
 
     this.setState({ activeItem: item, modal: !this.state.modal });
   };
